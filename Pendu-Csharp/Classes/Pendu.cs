2cc8f879869595433d601468f18510cf0c17c44d
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-15 21:45:04 +0200

    feat: ajout de la classe Pendu

---
+++ Pendu-Csharp/Classes/Pendu.cs
@@ -0,0 +1,20 @@
+﻿namespace Pendu_Csharp.Classes
+{
+    internal class Pendu
+    {
+        private string _masque;
+        private int _nbEssais;
+        private string _motATrouver;
+
+        public Pendu(string motATrouver, string masque, int nbEssais=10)
+        {
+            _motATrouver = motATrouver;
+            _masque = masque;
+            _nbEssais = nbEssais;
+        }
+
+        public void TestChar(char lettre) { }
+        public void TestWin() { }
+        public void GenererMasque() { }
+    }
+}
