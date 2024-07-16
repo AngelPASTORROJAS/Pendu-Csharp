6822690200601d0746d962cbc4ec3620dc58edb5
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-07-16 19:47:07 +0200

    feat: ajout de jeu en boucle

--- Pendu-Csharp/Program.cs
+++ Pendu-Csharp/Program.cs
@@ -1,4 +1,15 @@
 ﻿using Pendu_Csharp.Classes;
 
 Console.ForegroundColor = ConsoleColor.White;
-new Game();
+bool end = false;
+while (!end)
+{
+    new Game();
+    string modifyNbEssais = Utils.InputString("\nVoulez-vous rejouer ?Y/n\n", "Erreur de saisi!", true, ["y", "Y", "n", "N"]);
+    if (modifyNbEssais.ToUpper() == "N")
+    {
+        end = true;
+    }
+    Console.ReadKey();
+    Console.Clear();
+}
